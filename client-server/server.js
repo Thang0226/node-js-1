da635edb230588191257ccdb34c920ad5fc411a5
Thang0226 <thang.nd0226@gmail.com>
2025-01-27 16:36:49 +0700

    route different html response file to different request url

--- client-server/server.js
+++ client-server/server.js
@@ -1,12 +1,32 @@
 import http from "http";
+import fs from "fs";
 
 const server = http.createServer((req, resp) => {
-  //   console.log("request made");
   console.log(req.url, req.method, req.statusCode);
 
   resp.setHeader("Content-Type", "text/html"); // Response header
-  resp.write("<h1>Hello, Thang!</h1>"); // Response body
-  resp.end(); // End of response
+
+  let path = "./client-server/views/"; // "./" = project directory
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "error.html";
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      resp.end("<h1>404: Not found!</h1>");
+      return;
+    }
+    resp.write(data);
+    resp.end();
+  });
 });
 
 server.listen(3000, "localhost", () => {
